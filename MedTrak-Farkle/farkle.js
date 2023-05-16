bd5c968f446747609bf0975b925eea79baae8a25
Robin <robin.lynne.salomon@gmail.com>
2023-05-16 16:58:32 -0400

    Added confirm box to bankScore function

--- MedTrak-Farkle/farkle.js
+++ MedTrak-Farkle/farkle.js
@@ -27,30 +27,44 @@ function rollDice() {
       .getElementById("die6")
       .setAttribute("src", "./images/" + randomNumber6 + ".png");
 
-    var diceArr = [
-      randomNumber1,
-      randomNumber2,
-      randomNumber3,
-      randomNumber4,
-      randomNumber5,
-      randomNumber6,
-    ];
+    function determineScore() {
+      var diceArr = [
+        randomNumber1,
+        randomNumber2,
+        randomNumber3,
+        randomNumber4,
+        randomNumber5,
+        randomNumber6,
+      ];
+      for (var i = 0; i < diceArr.length; i++) {
+        //Scoring for rolling 1s
+        // const valueOfDiceIsOne = diceArr.filter((val) => val === 1);
+        //   if(valueOfDiceIsOne){
+        //     document.getElementById("header-text").innerHTML = "Y";
+        //   }
 
-    for (var i = 0; i < diceArr.length; i++) {
-      const isDuplicate = diceArr.filter((val) => val === diceArr[i]);
+        //Scoring for rolling three of a kind
 
-      if (isDuplicate.length === 3) {
-        var valueOfDice = isDuplicate[0];
-        document.getElementById("header-text").innerHTML = "Three of a kind!";
-        document.getElementById("score").innerHTML = valueOfDice * 100;
+        const isDuplicate = diceArr.filter((val) => val === diceArr[i]);
+        if (isDuplicate.length === 3) {
+          var valueOfDice = isDuplicate[0];
+          document.getElementById("header-text").innerHTML = "Three of a kind!";
+          document.getElementById("score").innerHTML = valueOfDice * 100;
+        }
       }
     }
+    determineScore();
   }, 100);
 }
 
 function bankScore() {
-  alert(
-    "Thanks for playing! Your total score is: " +
-      document.getElementById("score").innerHTML
-  );
+  if (
+    confirm(
+      "Your total score is: " +
+        document.getElementById("score").innerHTML +
+        ". By banking your score, you take home your points and end the game."
+    ) === true
+  ) {
+    document.getElementById("score").innerHTML = 0;
+  }
 }
